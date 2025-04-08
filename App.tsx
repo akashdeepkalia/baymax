beae75adeeb7b413a825539d657b262f1bbe94f1
Akashdeep Kalia <akashdeep1801333@gmail.com>
2025-04-08 14:53:31 +0530

    Temp commit before switching to main

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar } from 'react-native'
 import React from 'react'
 import Navigation from './src/navigation/Navigation'
 
 const App = () => {
   return (
-    <Navigation />
+    <>
+      <Navigation />
+      <StatusBar hidden />
+    </>
   )
 }
 
